b42629a5b3310ab437132e802890a0af2b913c65
ArshilHapani <arshilhapani998@gmail.com>
2024-04-24 22:21:35 +0530

    How to get event logs from deployed contract address......................

--- events_logging/scripts/getEvenLog.ts
+++ events_logging/scripts/getEvenLog.ts
@@ -3,7 +3,9 @@ import { SimpleStorage } from "../typechain-types";
 
 (async function () {
   const contractAddress =
-    "0xf61b9956ec5dffb6c6f5cfdd022f6275bf13bd8f8910593947d8ca03444c96dd";
+    "0xba54f5f7857f749ac83f3fdf226c5cdc470a10708abdca6bdfc0ab4958cefa2f"; // sepolia
+  // const contractAddress =
+  //   "0xcf68abb62f5b3a1e69e120425ed861189cb74b1b8a443ca1bdd21a46755d6842"; // ganache
 
   const receipt = await ethers.provider.getTransactionReceipt(contractAddress);
   const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
@@ -11,14 +13,13 @@ import { SimpleStorage } from "../typechain-types";
   const from = receipt?.blockNumber;
   const to = receipt?.blockNumber;
 
-  const eventABI = SimpleStorageFactory.interface.fragments.find(
-    (frag) => frag.type === "event"
-  );
-  const log =
-    (await receipt?.provider.getLogs({
-      fromBlock: from,
-      toBlock: to,
-    })) ?? [];
+  const log = await ethers.provider.getLogs({
+    address: "0xd3a9e6DD1609C97A30E5360CEB27a97dE81a5022",
+    fromBlock: from,
+    toBlock: to,
+    topics: [ethers.id("storeNumber")],
+  });
+  console.log(log);
 })();
 
 function hexToDec(hex: string) {
