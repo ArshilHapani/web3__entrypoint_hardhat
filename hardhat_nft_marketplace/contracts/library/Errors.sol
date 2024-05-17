e09152299bcd8e6669e5c7b0a6682ddc583c18e0
ArshilHapani <arshilhapani998@gmail.com>
2024-05-17 23:14:20 +0530

    Still win11 is not here :(

--- hardhat_nft_marketplace/contracts/library/Errors.sol
+++ hardhat_nft_marketplace/contracts/library/Errors.sol
@@ -4,4 +4,5 @@ pragma solidity ^0.8.19;
 library NFTMarketPlace__Errors {
     error PriceMustBeAboveZero();
     error NotApprovedForListing();
+    error ItemAlreadyListed(address nftAddress, uint256 tokenId);
 }
--- hardhat_nft_marketplace/contracts/library/Utils.sol
+++ hardhat_nft_marketplace/contracts/library/Utils.sol
@@ -1,11 +1,43 @@
 //SPDX-License-Identifier:MIT
 pragma solidity ^0.8.19;
 
+import {NFTMarketPlace__Errors} from "./Errors.sol";
+
 library Utils {
+    ///////////////////////
+    /////// Structures/////
+    ///////////////////////
+    struct Listing {
+        uint256 price;
+        address seller;
+    }
+
+    ///////////////////////
+    /////// Events/////////
+    ///////////////////////
     event ItemListed(
-        address indexed owner,
+        address indexed seller,
         address indexed nftAddress,
         uint256 indexed tokenId,
         uint256 price
     );
+
+    ///////////////////////
+    ////////Modifiers//////
+    ///////////////////////
+    modifier notListed(
+        address _nftAddress,
+        uint256 _tokenId,
+        address _owner,
+        mapping(address => mapping(uint256 => Listing)) storage s_listing
+    ) {
+        Listing memory listing = s_listing[_nftAddress][_tokenId];
+        if (listing.price > 0) {
+            revert NFTMarketPlace__Errors.ItemAlreadyListed(
+                _nftAddress,
+                _tokenId
+            );
+        }
+        _;
+    }
 }
